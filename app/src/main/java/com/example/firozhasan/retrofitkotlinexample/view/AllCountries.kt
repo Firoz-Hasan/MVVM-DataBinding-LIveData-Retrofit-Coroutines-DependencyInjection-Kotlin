15648825ddfef2526c6212be352ea44ccd51e853
firozhasan <diganto007@gmail.com>
2019-09-11 02:04:47 +0600

    all countries item design

---
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/AllCountries.kt
@@ -0,0 +1,13 @@
+package com.example.firozhasan.retrofitkotlinexample.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firozhasan.retrofitkotlinexample.R
+
+class AllCountries : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_countries)
+    }
+}
---
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/viewModel/AllCountiresViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.firozhasan.retrofitkotlinexample.viewModel
+
+import android.arch.lifecycle.ViewModel
+
+class AllCountiresViewModel : ViewModel() {
+}
