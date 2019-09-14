dba13159188bb3e25060ba87e5146945d305ed50
firozhasan <diganto007@gmail.com>
2019-09-14 06:17:08 +0600

    all countries frag working

--- app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/All_Countries_Frag.kt
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/All_Countries_Frag.kt
@@ -1,23 +1,55 @@
 package com.example.firozhasan.retrofitkotlinexample.view
 
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.firozhasan.retrofitkotlinexample.R
+import com.example.firozhasan.retrofitkotlinexample.model.modelClass.Country
+import com.example.firozhasan.retrofitkotlinexample.view.adapters.CountiresAdapter
+import com.example.firozhasan.retrofitkotlinexample.viewModel.AllCountiresViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class All_Countries_Frag : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: CountiresAdapter
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all__countries_, container, false)
+        var view = inflater.inflate(R.layout.activity_all_countries, container, false)
+
+        recyclerView = view?.findViewById(R.id.countries_RV)!!
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        val mainViewModel = ViewModelProviders.of(this)
+            .get(AllCountiresViewModel::class.java)
+        mainViewModel.onChangeFindCountryClick()
+
+        /*mainViewModel.allCountires.observe(this, Observer {
+            adapter = CountiresAdapter(this, heroList)
+            recyclerView.setAdapter(adapter)
+        })*/
+
+        mainViewModel.allCountires.observe(this, Observer<List<Country>> { t ->
+            adapter = CountiresAdapter(activity!!, t)
+            recyclerView.adapter = adapter
+        })
+        return view
     }
 
 
--- app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/MainActivity.kt
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/MainActivity.kt
@@ -1,30 +1,99 @@
 package com.example.firozhasan.retrofitkotlinexample.view
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
 import com.example.firozhasan.retrofitkotlinexample.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
+
+    var bottomNavigationView: BottomNavigationView? = null
+    var allCountriesFrag: All_Countries_Frag? = null
+    var searchFrag: Search_frag? = null
+    var toolbar: Toolbar? = null
+
+    // bottom navigation item selection
+    private val mOnNavigationItemSelectedListener =
+        object : BottomNavigationView.OnNavigationItemSelectedListener {
+
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.home -> {
+                        addFragment(allCountriesFrag!!, searchFrag!!)
+                        toolbar!!.visibility = View.VISIBLE
+                        return true
+                    }
+                    R.id.search -> {
+                        addFragment(searchFrag!!, allCountriesFrag!!)
+                        toolbar!!.visibility = View.INVISIBLE
+                        return true
+                    }
+                }
+                return false
+            }
+
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        prepareAllView()
+// add kountry list fragment as default fragment
+        addFragment(allCountriesFrag!!, searchFrag!!)
+        /*   val mainViewModel = ViewModelProviders.of(this)
+                   .get(CountryViewModel::class.java)
 
-     /*   val mainViewModel = ViewModelProviders.of(this)
-                .get(CountryViewModel::class.java)
-
-        DataBindingUtil.setContentView<ActivityMainBinding>(
-                this, R.layout.activity_main
-        ).apply {
-            this.setLifecycleOwner(this@MainActivity)
-            this.viewmodel = mainViewModel
-        }*/
+           DataBindingUtil.setContentView<ActivityMainBinding>(
+                   this, R.layout.activity_main
+           ).apply {
+               this.setLifecycleOwner(this@MainActivity)
+               this.viewmodel = mainViewModel
+           }*/
 
 
     }
 
+    fun prepareAllView() {
+        prepareToolBar()
+        prepareBottomNavView()
+        allCountriesFrag = All_Countries_Frag()
+        searchFrag = Search_frag()
+    }
 
+    fun prepareToolBar() {
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
 
+    fun prepareBottomNavView() {
+        bottomNavigationView = findViewById(R.id.navigation_bottom)
+        bottomNavigationView!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun addFragment(fragment: Fragment, fragment2: Fragment) {
+        if (fragment.isAdded) {
+            supportFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out)
+                .hide(fragment2)
+                .show(fragment)
+                .commit()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out)
+                .hide(fragment2)
+                .add(R.id.content, fragment, fragment.javaClass.getSimpleName())
+                .show(fragment)
+                .commit()
+        }
+
+    }
 
 
 }
