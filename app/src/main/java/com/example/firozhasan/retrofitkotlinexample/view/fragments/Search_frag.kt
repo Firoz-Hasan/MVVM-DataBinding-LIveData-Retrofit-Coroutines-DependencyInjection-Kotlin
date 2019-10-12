6dedd5535a15977d3a66f081a25cd60f11e1c81f
firozhasan <diganto007@gmail.com>
2019-10-12 19:27:35 +0600

    find country by capital e inside e jacche nah

--- app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/fragments/Search_frag.kt
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/view/fragments/Search_frag.kt
@@ -10,18 +10,33 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.firozhasan.retrofitkotlinexample.R
 import com.example.firozhasan.retrofitkotlinexample.databinding.FragmentSearchFragBinding
+import com.example.firozhasan.retrofitkotlinexample.viewModel.AllCountriesViewModel
 import com.example.firozhasan.retrofitkotlinexample.viewModel.CountryViewModel
+import com.example.firozhasan.retrofitkotlinexample.viewModel.FindCountryViewModel
+import com.example.firozhasan.retrofitkotlinexample.viewModel.FindCountryViewModelFactory
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+import org.kodein.di.generic.instance
 
 /**
  * A simple [Fragment] subclass.
  */
-class Search_frag : Fragment() {
+class Search_frag : Fragment(),KodeinAware {
+
+    private lateinit var viewModel: FindCountryViewModel
+    override val kodein by kodein()
+    private val factory: FindCountryViewModelFactory by instance()
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       val viewModel = ViewModelProviders.of(this)
-            .get(CountryViewModel::class.java)
+       //val viewModel = ViewModelProviders.of(this).get(FindCountryViewModel::class.java)
+
+        viewModel = ViewModelProviders.of(this, factory).get(FindCountryViewModel::class.java)
         val binding = DataBindingUtil.inflate<FragmentSearchFragBinding>(inflater,
             R.layout.fragment_search_frag, container, false).apply {
             this.lifecycleOwner = activity
