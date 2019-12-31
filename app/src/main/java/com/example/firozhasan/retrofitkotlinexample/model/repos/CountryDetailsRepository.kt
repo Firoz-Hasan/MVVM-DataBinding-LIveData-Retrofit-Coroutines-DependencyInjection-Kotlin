e8efb8912fd8d02f537e3605c3cf831e6f17ec26
firozhasan <diganto007@gmail.com>
2020-01-01 00:05:40 +0600

    still have issues

--- app/src/main/java/com/example/firozhasan/retrofitkotlinexample/model/repos/CountryDetailsRepository.kt
+++ app/src/main/java/com/example/firozhasan/retrofitkotlinexample/model/repos/CountryDetailsRepository.kt
@@ -9,7 +9,7 @@ import com.example.firozhasan.retrofitkotlinexample.model.modelClass.Country
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class CountryDetailsRepository (private val countriesAPI: CountriesAPI) : SafeApiRequest() {
+class CountryDetailsRepository(private val countriesAPI: CountriesAPI) : SafeApiRequest() {
 
 
     val findCountryFullInfo = MutableLiveData<List<Country>>()
@@ -22,6 +22,8 @@ class CountryDetailsRepository (private val countriesAPI: CountriesAPI) : SafeAp
     val detailsCurrency = MutableLiveData<String>()
     val detailsLanguage = MutableLiveData<String>()
     val detailsFlag = MutableLiveData<String>()
+    val detailsLat = MutableLiveData<String>()
+    val detailsLng = MutableLiveData<String>()
 
 
     suspend fun getCountryDetails(alpha2Code: String): LiveData<List<Country>> {
@@ -36,7 +38,7 @@ class CountryDetailsRepository (private val countriesAPI: CountriesAPI) : SafeAp
 
     private suspend fun fetchCountryDetails(alpha2Code: String) {
         //val lastSavedAt = prefs.getLastSavedAt()
-        Log.d("hellohello","fetchCountryDetails outside fetch ")
+        Log.d("hellohello", "fetchCountryDetails outside fetch ")
         try {
 
             val response = apiRequest { countriesAPI.getCountryDetails(alpha2Code) }
@@ -49,14 +51,16 @@ class CountryDetailsRepository (private val countriesAPI: CountriesAPI) : SafeAp
             detailsTimezone.postValue(response.timezones!![0])
             detailsCurrency.postValue(response.currencies!![0]?.name)
             detailsLanguage.postValue(response.languages!![0]?.name)
-
-            Log.d("hellohello","fetchCountryDetails fetch try+${response.flag} ")
+            detailsLat.postValue(response.latlng?.get(0).toString())
+            detailsLng.postValue(response.latlng?.get(1).toString())
+            Log.d("hellohello", "fetchCountryDetails fetch try+${response.flag} " +
+                    "anf ${response.latlng?.get(0).toString()}")
             //findCountryFullInfo.postValue(response)
             //findCountryName.postValue(response[0].name)
 
         } catch (e: Exception) {
             e.printStackTrace()
-            Log.d("hellohello","fetchAllCountries2inside fetch catch+${e.toString()} ")
+            Log.d("hellohello", "fetchAllCountries2inside fetch catch+${e.toString()} ")
 
         }
 
